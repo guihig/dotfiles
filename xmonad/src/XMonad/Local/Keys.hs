95116598fe0861ef2f74f061f60332e6a5169bb5
guihig <guiih.ig@gmail.com>
2021-03-29 11:24:01 -0300

    att

--- xmonad/src/XMonad/Local/Keys.hs
+++ xmonad/src/XMonad/Local/Keys.hs
@@ -2,11 +2,10 @@
 {-# OPTIONS_HADDOCK hide, prune, ignore-exports #-}
 
 module XMonad.Local.Keys
-  ( keys,
-    rawKeys,
-    additionalKeys,
-  )
-where
+  ( keys
+  , rawKeys
+  , additionalKeys
+  ) where
 
 --------------------------------------------------------------------------------
 -- General Haskell Packages.
@@ -15,66 +14,48 @@ import Graphics.X11.Xlib
 import System.Directory
 import System.Exit (exitSuccess)
 import System.FilePath ((</>))
+
 --------------------------------------------------------------------------------
 -- Package: xmonad.
 import XMonad hiding (keys)
+
 --------------------------------------------------------------------------------
 -- Package: xmonad-contrib.
 import XMonad.Actions.CopyWindow (kill1)
 import XMonad.Actions.DynamicProjects (switchProjectPrompt)
+
 --------------------------------------------------------------------------------
 -- Local modules.
-
 import XMonad.Actions.FloatKeys (keysResizeWindow)
 import XMonad.Actions.Minimize
 import XMonad.Actions.Navigation2D
-import XMonad.Actions.PhysicalScreens
-  ( onNextNeighbour,
-    onPrevNeighbour,
-  )
+import XMonad.Actions.PhysicalScreens (onNextNeighbour, onPrevNeighbour)
 import XMonad.Actions.Promote (promote)
 import XMonad.Actions.SinkAll (sinkAll)
 import XMonad.Actions.SwapPromote (swapHybrid)
-import XMonad.Actions.TagWindows
-  ( addTag,
-    delTag,
-    withTagged,
-  )
+import XMonad.Actions.TagWindows (addTag, delTag, withTagged)
 import XMonad.Actions.UpdatePointer (updatePointer)
-import XMonad.Hooks.ManageDocks (ToggleStruts (..))
+import XMonad.Hooks.ManageDocks (ToggleStruts(..))
 import XMonad.Hooks.UrgencyHook (focusUrgent)
-import XMonad.Layout.IndependentScreens
-  ( onCurrentScreen,
-  )
+import XMonad.Layout.IndependentScreens (onCurrentScreen)
 import XMonad.Layout.Maximize (maximizeRestore)
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
-import XMonad.Layout.SubLayouts
-  ( GroupMsg (MergeAll, UnMerge),
-    pullGroup,
-    toSubl,
-  )
-import XMonad.Layout.ToggleLayouts (ToggleLayout (..))
-import XMonad.Layout.ZoomRow
-  ( zoomIn,
-    zoomOut,
-    zoomReset,
-  )
+import XMonad.Layout.SubLayouts (GroupMsg(MergeAll, UnMerge), pullGroup, toSubl)
+import XMonad.Layout.ToggleLayouts (ToggleLayout(..))
+import XMonad.Layout.ZoomRow (zoomIn, zoomOut, zoomReset)
 import XMonad.Local.Applications as Applications
-import XMonad.Local.Layout
-  ( selectLayoutByName,
-    toggleLayout,
-  )
+import XMonad.Local.Layout (selectLayoutByName, toggleLayout)
 import qualified XMonad.Local.Theme as Local
 import XMonad.Local.Workspaces (scratchPads)
 import XMonad.Prompt
 import XMonad.Prompt.ConfirmPrompt (confirmPrompt)
 import XMonad.Prompt.Shell (shellPrompt)
 import XMonad.Prompt.Window
-  ( WindowPrompt (..),
-    allWindows,
-    windowMultiPrompt,
-    wsWindows,
+  ( WindowPrompt(..)
+  , allWindows
+  , windowMultiPrompt
+  , wsWindows
   )
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig (mkKeymap)
@@ -89,23 +70,21 @@ additionalKeys :: [((KeyMask, KeySym), X ())]
 additionalKeys = workspaceKeys'
 
 --------------------------------------------------------------------------------
-
 -- | Access the unprocessed key meant to be fed into @mkKeymap@.
 rawKeys :: XConfig Layout -> [(String, X ())]
 rawKeys c = withUpdatePointer $ concatMap ($ c) keymaps
   where
     keymaps =
-      [ baseKeys,
-        windowKeys,
-        workspaceKeys,
-        layoutKeys,
-        screenKeys,
-        appKeys,
-        systemKeys
+      [ baseKeys
+      , windowKeys
+      , workspaceKeys
+      , layoutKeys
+      , screenKeys
+      , appKeys
+      , systemKeys
       ]
 
 --------------------------------------------------------------------------------
-
 -- | Modify all keybindings so that after they finish their action the
 -- mouse pointer is moved to the corner of the focused window.  This
 -- is a bit of a hack to work around some issues I have with
@@ -117,32 +96,29 @@ withUpdatePointer = map addAction
     addAction (key, action) = (key, action >> updatePointer (0.75, 0.25) (0, 0))
 
 --------------------------------------------------------------------------------
--- Specifically manage my prefix key (C-z), and for controlling XMonad.
 baseKeys :: XConfig Layout -> [(String, X ())]
 baseKeys c =
-  [ ("M-q", spawn "xmonad --restart"),
-    ("M-C-q", spawn "xmonad --recompile && xmonad --restart"),
-    ("M-S-q", confirmPrompt Local.promptConfig "Quit XMonad" $ io exitSuccess)
+  [ ("M-q", broadcastMessage ReleaseResources >> restart "xmonad" True)
+  , ("M-S-q", confirmPrompt Local.promptConfig "Quit XMonad" $ io exitSuccess)
   ]
 
 --------------------------------------------------------------------------------
 -- Specifically manage my prefix key (C-z), and for controlling XMonad.
 systemKeys :: XConfig Layout -> [(String, X ())]
 systemKeys _ =
-  [ ("S-<Print>", spawn "sleep 0.2 && gnome-screenshot -a"),
-    ("M-v", spawn "polybar-msg cmd toggle"),
-    ("M-=", spawn "$HOME/.xmonad/scripts/lock.sh")
+  [ ("S-<Print>", spawn "sleep 0.2 && gnome-screenshot -a")
+  , ("M-v", spawn "polybar-msg cmd toggle")
+  , ("M-=", spawn "$HOME/.xmonad/scripts/lock.sh")
   ]
 
 --------------------------------------------------------------------------------
 toggleFloat :: Window -> X ()
 toggleFloat w =
   windows
-    ( \s ->
-        if M.member w $ W.floating s
-          then W.sink w s
-          else W.float w (W.RationalRect (1 / 3) (1 / 4) (1 / 2) (4 / 5)) s
-    )
+    (\s ->
+       if M.member w $ W.floating s
+         then W.sink w s
+         else W.float w (W.RationalRect (1 / 3) (1 / 4) (1 / 2) (4 / 5)) s)
 
 -- Window focusing, swapping, and other actions.
 directions2D :: [Direction2D]
@@ -151,66 +127,62 @@ directions2D = [D, U, L, R]
 arrowKeys :: [String]
 arrowKeys = ["j", "k", "h", "l"]
 
-data ResizeModes = RShrink | RExpand | RMirrorShrink | RMirrorExpand deriving (Show, Eq)
+data ResizeModes
+  = RShrink
+  | RExpand
+  | RMirrorShrink
+  | RMirrorExpand
+  deriving (Show, Eq)
 
 resizeHorizontal :: ResizeModes -> X ()
 resizeHorizontal mode =
   withFocused
-    ( \w -> do
-        floats <- gets (W.floating . windowset)
-        if M.member w floats
-          then
-            if mode == RShrink
-              then keysResizeWindow (-10, 0) (0, 0) w
-              else keysResizeWindow (10, 0) (0, 0) w
-          else
-            if mode == RShrink
-              then sendMessage Shrink
-              else sendMessage Expand
-    )
+    (\w -> do
+       floats <- gets (W.floating . windowset)
+       if M.member w floats
+         then if mode == RShrink
+                then keysResizeWindow (-10, 0) (0, 0) w
+                else keysResizeWindow (10, 0) (0, 0) w
+         else if mode == RShrink
+                then sendMessage Shrink
+                else sendMessage Expand)
 
 resizeVertical :: ResizeModes -> X ()
 resizeVertical mode =
   withFocused
-    ( \w -> do
-        floats <- gets (W.floating . windowset)
-        if M.member w floats
-          then
-            if mode == RMirrorShrink
-              then keysResizeWindow (0, 10) (0, 0) w
-              else keysResizeWindow (0, -10) (0, 0) w
-          else
-            if mode == RMirrorShrink
-              then sendMessage MirrorShrink
-              else sendMessage MirrorExpand
-    )
+    (\w -> do
+       floats <- gets (W.floating . windowset)
+       if M.member w floats
+         then if mode == RMirrorShrink
+                then keysResizeWindow (0, 10) (0, 0) w
+                else keysResizeWindow (0, -10) (0, 0) w
+         else if mode == RMirrorShrink
+                then sendMessage MirrorShrink
+                else sendMessage MirrorExpand)
 
 windowKeys :: XConfig Layout -> [(String, X ())]
-windowKeys _ =
+windowKeys _
   -- Focusing Windows:
-  [ ("M-,", windows W.focusUp),
-    ("M-.", windows W.focusDown),
-    ("M-S-,", windows W.swapUp),
-    ("M-S-.", windows W.swapDown),
-    ("M-u", focusUrgent),
-    ("M-o", windowPromptGoto),
-    ("M-C-m", windows W.focusMaster),
-    ("M-[", resizeHorizontal RShrink),
-    ("M-]", resizeHorizontal RExpand),
-    ("M-S-[", resizeVertical RMirrorExpand),
-    ("M-S-]", resizeVertical RMirrorShrink),
-    ("M-w", kill1),
-    ("M--", withFocused minimizeWindow >> windows W.focusDown),
-    ("M-S--", withLastMinimized maximizeWindowAndFocus)
-  ]
-    ++
+ =
+  [ ("M-,", windows W.focusUp)
+  , ("M-.", windows W.focusDown)
+  , ("M-S-,", windows W.swapUp)
+  , ("M-S-.", windows W.swapDown)
+  , ("M-u", focusUrgent)
+  , ("M-o", windowPromptGoto)
+  , ("M-C-m", windows W.focusMaster)
+  , ("M-[", resizeHorizontal RShrink)
+  , ("M-]", resizeHorizontal RExpand)
+  , ("M-S-[", resizeVertical RMirrorExpand)
+  , ("M-S-]", resizeVertical RMirrorShrink)
+  , ("M-w", kill1)
+  , ("M--", withFocused minimizeWindow >> windows W.focusDown)
+  , ("M-S--", withLastMinimized maximizeWindowAndFocus)
+  ] ++
     -- Navigation Windows
-    [("M-" ++ k, windowGo d True) | (k, d) <- zip arrowKeys directions2D]
-    ++
+  [("M-" ++ k, windowGo d True) | (k, d) <- zip arrowKeys directions2D] ++
     -- Navigation Windows Move
-    [ ("M-C-" ++ k, windowSwap d False)
-      | (k, d) <- zip arrowKeys directions2D
-    ]
+  [("M-C-" ++ k, windowSwap d False) | (k, d) <- zip arrowKeys directions2D]
 
 --------------------------------------------------------------------------------
 -- Keys for manipulating workspaces.
@@ -220,58 +192,54 @@ workspaceKeys _ = [("M-p", switchProjectPrompt Local.promptConfig)]
 workspaceKeys' :: [((KeyMask, KeySym), X ())]
 workspaceKeys' =
   [ ((m .|. mod4Mask, k), windows $ onCurrentScreen f (show i))
-    | (i, k) <- zip [0 .. 9] [xK_1 .. xK_9],
-      (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+  | (i, k) <- zip [0 .. 9] [xK_1 .. xK_9]
+  , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
   ]
 
 --------------------------------------------------------------------------------
 -- Layout switching and manipulation.
 layoutKeys :: XConfig Layout -> [(String, X ())]
 layoutKeys c =
-  [ ("M-<Backspace>", selectLayoutByName Local.promptConfig),
-    ( "M-f",
-      sequence_ [withFocused $ windows . W.sink, sendMessage ToggleLayout]
-    ),
-    ("M-\\", toggleLayout "Tall"),
-    ("M-t s", sendMessage ToggleStruts),
-    ("M-y", withFocused toggleFloat),
-    ("M-S-y", sinkAll),
-    ("M-<Tab>", sendMessage NextLayout),
-    ("M-S-<Tab>", toSubl NextLayout),
-    ("M-g", withFocused (sendMessage . UnMerge)),
-    ("M-S-g", withFocused (sendMessage . MergeAll))
-  ]
-    ++
+  [ ("M-<Backspace>", selectLayoutByName Local.promptConfig)
+  , ( "M-f"
+    , sequence_ [withFocused $ windows . W.sink, sendMessage ToggleLayout])
+  , ("M-\\", toggleLayout "Tall")
+  , ("M-t s", sendMessage ToggleStruts)
+  , ("M-y", withFocused toggleFloat)
+  , ("M-S-y", sinkAll)
+  , ("M-<Tab>", sendMessage NextLayout)
+  , ("M-S-<Tab>", toSubl NextLayout)
+  , ("M-g", withFocused (sendMessage . UnMerge))
+  , ("M-S-g", withFocused (sendMessage . MergeAll))
+  ] ++
     -- Sublayout Merge Windows
-    [ ("M-S-C-" ++ k, sendMessage $ pullGroup d)
-      | (k, d) <- zip arrowKeys directions2D
-    ]
+  [ ("M-S-C-" ++ k, sendMessage $ pullGroup d)
+  | (k, d) <- zip arrowKeys directions2D
+  ]
 
 --------------------------------------------------------------------------------
 -- Keys to manipulate screens (actual physical monitors).
 screenKeys :: XConfig Layout -> [(String, X ())]
 screenKeys _ =
-  [ ("M-;", onNextNeighbour def W.view),
-    ("M-S-;", onPrevNeighbour def W.view),
-    ("M-C-;", screenSwap L True)
-  ]
-    ++ [ ("M-S-" ++ k, windowToScreen d False)
-         | (k, d) <- zip arrowKeys directions2D
-       ]
+  [ ("M-;", onNextNeighbour def W.view)
+  , ("M-S-;", onPrevNeighbour def W.view)
+  , ("M-C-;", screenSwap L True)
+  ] ++
+  [("M-S-" ++ k, windowToScreen d False) | (k, d) <- zip arrowKeys directions2D]
 
 --------------------------------------------------------------------------------
 -- Keys for launching applications.
 appKeys :: XConfig Layout -> [(String, X ())]
 appKeys _c =
-  [ ("M-<Return>", spawn Applications.terminal),
-    ("M-<Esc>", shellPrompt Local.promptConfig),
-    ("M-<Space>", spawn Applications.launcher),
-    ("M-s", namedScratchpadAction scratchPads "spotify"),
+  [ ("M-<Return>", spawn Applications.terminal)
+  , ("M-<Esc>", shellPrompt Local.promptConfig)
+  , ("M-<Space>", spawn Applications.launcher)
+  , ("M-s", namedScratchpadAction scratchPads "spotify")
     -- ("M-d", namedScratchpadAction scratchPads "slack"),
-    ("M-a", namedScratchpadAction scratchPads "discord"),
-    ("M-j", namedScratchpadAction scratchPads "joplin"),
-    ("M-r", namedScratchpadAction scratchPads "mailspring"),
-    ("M-e", spawn Applications.files)
+  , ("M-a", namedScratchpadAction scratchPads "discord")
+  , ("M-j", namedScratchpadAction scratchPads "joplin")
+  , ("M-r", namedScratchpadAction scratchPads "mailspring")
+  , ("M-e", spawn Applications.files)
   ]
 
 --------------------------------------------------------------------------------
@@ -279,8 +247,8 @@ windowPromptGoto :: X ()
 windowPromptGoto = windowMultiPrompt Local.promptConfig modes
   where
     modes =
-      [ (Goto, allWindows),
-        (Goto, wsWindows),
-        (BringCopy, allWindows),
-        (Bring, allWindows)
+      [ (Goto, allWindows)
+      , (Goto, wsWindows)
+      , (BringCopy, allWindows)
+      , (Bring, allWindows)
       ]
--- xmonad/xmonad-guihig.hs
+++ xmonad/xmonad-guihig.hs
@@ -2,64 +2,61 @@
 
 module Main where
 
-import           XMonad                  hiding ( config )
-import           XMonad.Actions.DynamicProjects ( dynamicProjects )
-import           XMonad.Actions.Navigation2D
-import           XMonad.Hooks.UrgencyHook
-                                         hiding ( urgencyConfig )
-import           XMonad.Hooks.EwmhDesktops      ( ewmh )
+import XMonad hiding (config)
+import XMonad.Actions.DynamicProjects (dynamicProjects)
+import XMonad.Actions.Navigation2D
+import XMonad.Hooks.EwmhDesktops (ewmh)
+import XMonad.Hooks.UrgencyHook hiding (urgencyConfig)
 
-import           XMonad.Util.EZConfig           ( additionalKeys )
-import           XMonad.Util.NamedScratchpad    ( namedScratchpadManageHook )
-import           XMonad.Util.Run                ( spawnPipe )
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.NamedScratchpad (namedScratchpadManageHook)
+import XMonad.Util.Run (spawnPipe)
 
-import qualified XMonad.Local.Action           as Local
-import qualified XMonad.Local.Keys             as Local
-import qualified XMonad.Local.Layout           as Local
-import qualified XMonad.Local.Log              as Local
-import qualified XMonad.Local.Theme            as Local
-import qualified XMonad.Local.Workspaces       as Workspaces
-import qualified XMonad.Local.Applications     as Applications
-import qualified XMonad.Local.Startup          as Startup
-
+import qualified XMonad.Local.Action as Local
+import qualified XMonad.Local.Applications as Applications
+import qualified XMonad.Local.Keys as Local
+import qualified XMonad.Local.Layout as Local
+import qualified XMonad.Local.Log as Local
+import qualified XMonad.Local.Startup as Startup
+import qualified XMonad.Local.Theme as Local
+import qualified XMonad.Local.Workspaces as Workspaces
 
 config =
-  ewmh def
-      { terminal          = Applications.terminal
-      , layoutHook        = Local.layoutHook
-      , manageHook        = namedScratchpadManageHook Workspaces.scratchPads
-                              <> Local.manageHook
-      , handleEventHook   = Local.handleEventHook
-      , logHook           = Local.logHook
-      , workspaces        = Workspaces.names
-      , modMask           = mod4Mask
-      , keys              = Local.keys
+  ewmh
+    def
+      { terminal = Applications.terminal
+      , layoutHook = Local.layoutHook
+      , manageHook =
+          namedScratchpadManageHook Workspaces.scratchPads <> Local.manageHook
+      , handleEventHook = Local.handleEventHook
+      , logHook = Local.logHook
+      , workspaces = Workspaces.names
+      , modMask = mod4Mask
+      , keys = Local.keys
       , focusFollowsMouse = False
-      , startupHook       = Startup.startupHook
-      }
-    `additionalKeys` Local.additionalKeys
-
+      , startupHook = Startup.startupHook
+      } `additionalKeys`
+  Local.additionalKeys
 
 navConf :: Navigation2DConfig
-navConf = def
-  { defaultTiledNavigation = hybridOf sideNavigation centerNavigation
-  , floatNavigation        = centerNavigation
-  , screenNavigation       = lineNavigation
-  , layoutNavigation       = [("Full", centerNavigation)]
-  , unmappedWindowRect     = [("Full", singleWindowRect)]
-  }
+navConf =
+  def
+    { defaultTiledNavigation = hybridOf sideNavigation centerNavigation
+    , floatNavigation = centerNavigation
+    , screenNavigation = lineNavigation
+    , layoutNavigation = [("Full", centerNavigation)]
+    , unmappedWindowRect = [("Full", singleWindowRect)]
+    }
 
 -- Main
 main :: IO ()
 main = do
   _ <- spawnPipe Applications.statusBar
-
-  xmonad
-    $ ewmh
-    $ dynamicProjects Workspaces.projects
-    $ withUrgencyHookC urgencyStyle urgencyConfig
-    $ withNavigation2DConfig navConf
-    $ Local.xmonadColors config
- where
-  urgencyConfig = UrgencyConfig Focused Dont
-  urgencyStyle  = BorderUrgencyHook "#ff0000"
+  xmonad $
+    ewmh $
+    dynamicProjects Workspaces.projects $
+    withUrgencyHookC urgencyStyle urgencyConfig $
+    withNavigation2DConfig navConf $ Local.xmonadColors config
+  where
+    urgencyConfig = UrgencyConfig Focused Dont
+    urgencyStyle = BorderUrgencyHook "#ff0000"
