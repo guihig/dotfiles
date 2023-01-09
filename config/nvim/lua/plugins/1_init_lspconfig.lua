cffcba348e42916f57f4e1a078dc69df243f867e
guihig <guiih.ig@gmail.com>
2023-01-09 13:56:40 -0300

    att

--- config/nvim/lua/plugins/1_init_lspconfig.lua
+++ config/nvim/lua/plugins/1_init_lspconfig.lua
@@ -1,107 +1,7 @@
 local keymap = vim.keymap
 local lspconfig = require "lspconfig"
 local utils = require("utils")
-local configs = require "lspconfig.configs"
-local lsp_util = require "lspconfig/util"
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
--- Setup Volar Multiple svrs cfg
--- local function on_new_config(new_config, new_root_dir)
---     local function get_typescript_server_path(root_dir)
---         local project_root = lsp_util.find_node_modules_ancestor(root_dir)
---         return project_root
---                    and (lsp_util.path
---                        .join(project_root, "node_modules", "typescript", "lib",
---                              "tsserverlibrary.js")) or ""
---     end
---
---     if new_config.init_options and new_config.init_options.typescript
---         and new_config.init_options.typescript.serverPath == "" then
---         new_config.init_options.typescript.serverPath =
---             get_typescript_server_path(new_root_dir)
---     end
--- end
---
--- local volar_cmd = { "vue-language-server", "--stdio" }
--- local volar_root_dir = lsp_util.root_pattern "package.json"
---
--- if not configs.volar_api then
---     configs.volar_api = {
---         default_config = {
---             cmd = volar_cmd,
---             root_dir = volar_root_dir,
---             on_new_config = on_new_config,
---             filetypes = { "vue" },
---             init_options = {
---                 typescript = { serverPath = "" },
---                 languageFeatures = {
---                     implementation = true,
---                     references = true,
---                     definition = true,
---                     typeDefinition = true,
---                     callHierarchy = true,
---                     hover = true,
---                     rename = true,
---                     renameFileRefactoring = true,
---                     signatureHelp = true,
---                     codeAction = true,
---                     workspaceSymbol = true,
---                     completion = {
---                         defaultTagNameCase = "both",
---                         defaultAttrNameCase = "kebabCase",
---                         getDocumentNameCasesRequest = false,
---                         getDocumentSelectionRequest = false
---                     }
---                 }
---             }
---         }
---     }
--- end
---
--- if not configs.volar_doc then
---     configs.volar_doc = {
---         default_config = {
---             cmd = volar_cmd,
---             root_dir = volar_root_dir,
---             on_new_config = on_new_config,
---             filetypes = { "vue" },
---             init_options = {
---                 typescript = { serverPath = "" },
---                 languageFeatures = {
---                     implementation = true,
---                     documentHighlight = true,
---                     documentLink = true,
---                     codeLens = false,
---                     semanticTokens = false,
---                     diagnostics = true,
---                     schemaRequestService = true
---                 }
---             }
---         }
---     }
--- end
---
--- if not configs.volar_html then
---     configs.volar_html = {
---         default_config = {
---             cmd = volar_cmd,
---             root_dir = volar_root_dir,
---             on_new_config = on_new_config,
---             filetypes = { "vue" },
---             init_options = {
---                 typescript = { serverPath = "" },
---                 documentFeatures = {
---                     selectionRange = true,
---                     foldingRange = true,
---                     linkedEditingRange = true,
---                     documentSymbol = true,
---                     documentColor = false,
---                     documentFormatting = { defaultPrintWidth = 100 }
---                 }
---             }
---         }
---     }
--- end
 
 -- Helpers
 local function lsp_organize_imports()
@@ -270,7 +170,7 @@ local servers = {
             }
         }
     },
-    tsserver = { flags = { debounce_text_changes = 50 } },
+    -- tsserver = { flags = { debounce_text_changes = 50 } },
     texlab = {
         settings = {
             texlab = {
@@ -307,7 +207,16 @@ local servers = {
     -- volar_api = {},
     -- volar_doc = {},
     -- volar_html = {},
-    volar = {},
+    volar = {
+        filetypes = {
+            "typescript",
+            "javascript",
+            "javascriptreact",
+            "typescriptreact",
+            "vue",
+            "json"
+        }
+    },
     yamlls = {},
     asm_lsp = {},
     rust_analyzer = {},
--- config/nvim/lua/plugins/init_rnvimr.lua
+++ config/nvim/lua/plugins/init_rnvimr.lua
@@ -1,32 +1,33 @@
 local keymap = vim.keymap
 
-local opts = {noremap = true, silent = true}
-    keymap.set('t', '<A-i>', '<C-\\><C-n>:RnvimrResize<CR>', opts)
-    keymap.set('n', '<A-q>', ':RnvimrToggle<CR>', opts)
-    keymap.set('t', '<A-q>', '<C-\\><C-n>:RnvimrToggle<CR>', opts)
+local opts = { noremap = true, silent = true }
+keymap.set("t", "<A-i>", "<C-\\><C-n>:RnvimrResize<CR>", opts)
+keymap.set("n", "<A-q>", ":RnvimrToggle<CR>", opts)
+keymap.set("t", "<A-q>", "<C-\\><C-n>:RnvimrToggle<CR>", opts)
 
-    vim.g.loaded_netrw = 1
-    vim.g.netrw_loaded_netrwPlugin = 1
-    vim.g.loaded_netrwPlugin = 1
-    vim.g.rnvimr_enable_ex = 0
-    vim.g.rnvimr_enable_picker = 1
-    vim.g.rnvimr_hide_gitignore = 1
-    vim.g.rnvimr_draw_border = "both"
-    vim.g.rnvimr_border_attr = {fg = 14, bg = -1}
-    vim.g.rnvimr_enable_bw = 1
-    vim.g.rnvimr_shadow_winblend = 100
-    vim.g.rnvimr_action = {
-        ['<C-t>'] = 'NvimEdit tabedit',
-        ['<C-x>'] = 'NvimEdit split',
-        ['<C-v>'] = 'NvimEdit vsplit',
-        ['gw'] = 'JumpNvimCwd',
-        ['yw'] = 'EmitRangerCwd'
-    }
-    vim.g.rnvimr_presets = {
-        {['width'] = 0.600, ['height'] = 0.600},
-        {['width'] = 0.800, ['height'] = 0.800},
-        {['width'] = 0.950, ['height'] = 0.950}
-    }
+-- vim.g.loaded_netrw = 1
+-- vim.g.netrw_loaded_netrwPlugin = 1
+-- vim.g.loaded_netrwPlugin = 1
+
+vim.g.rnvimr_enable_ex = 1
+vim.g.rnvimr_enable_picker = 1
+vim.g.rnvimr_hide_gitignore = 1
+vim.g.rnvimr_draw_border = "both"
+vim.g.rnvimr_border_attr = { fg = 14, bg = -1 }
+vim.g.rnvimr_enable_bw = 1
+vim.g.rnvimr_shadow_winblend = 100
+vim.g.rnvimr_action = {
+    ["<C-t>"] = "NvimEdit tabedit",
+    ["<C-x>"] = "NvimEdit split",
+    ["<C-v>"] = "NvimEdit vsplit",
+    ["gw"] = "JumpNvimCwd",
+    ["yw"] = "EmitRangerCwd"
+}
+vim.g.rnvimr_presets = {
+    { ["width"] = 0.600, ["height"] = 0.600 },
+    { ["width"] = 0.800, ["height"] = 0.800 },
+    { ["width"] = 0.950, ["height"] = 0.950 }
+}
 
 vim.cmd([[
   highlight link RnvimrNormal CursorLine
