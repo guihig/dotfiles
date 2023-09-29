d86ecbd38706b0db0a6d70d6e5763477e0c53980
guihig <guiih.ig@gmail.com>
2023-09-29 20:37:35 -0300

    Init nixos config

---
+++ home-manager/home.nix
@@ -0,0 +1,453 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+    inputs.sops-nix.homeManagerModule
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "ferreira";
+    homeDirectory = "/home/ferreira";
+  };
+
+  home.packages = with pkgs; [
+    # Very Fun and games
+    firefox
+    google-chrome
+    chromium
+    spotify
+    discord
+
+    # System utilities
+    htop
+    ncdu
+    unzip
+    jq
+    ranger
+    ripgrep
+    fd
+    killall
+    pulseaudio
+    fzf
+    exa
+    xcowsay
+    xclip
+    gnome.gnome-disk-utility
+    sl
+    neofetch
+    cmatrix
+    libnotify
+    gnome.nautilus
+    gnome.seahorse
+    gparted
+    flameshot
+    gzip
+    libsForQt5.ark
+    
+    # soundsss
+    pavucontrol
+    alsa-utils
+
+    # networking
+    openfortivpn
+    networkmanagerapplet
+
+    # very cool code kid0
+    vim
+    nano
+    vscode
+    cargo
+    rustc
+    go
+    gcc
+    lua
+    nodejs
+    (let
+      python3-with-packages = pkgs.python3.withPackages (p:
+        with p; [
+          pynvim
+          setuptools
+        ]);
+    in
+      python3-with-packages)
+    jetbrains.pycharm-professional
+    docker-compose
+    postgresql_13
+
+    # Fonts
+    nerdfonts
+    jetbrains-mono
+    iosevka
+    iosevka-bin
+    siji
+    termsyn
+    material-icons
+    material-design-icons
+    material-symbols
+    terminus_font
+    terminus-nerdfont
+    fantasque-sans-mono
+    noto-fonts
+  ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+
+  # ---- Git Configuration ---- #
+  programs.git = {
+    enable = true;
+    aliases = {
+      s = "status";
+      a = "add";
+      p = "push";
+      c = "commit";
+    };
+  };
+
+  # TODO: Change to fish
+  # ---- zsh Configuration ---- #
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    historySubstringSearch.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "fzf"
+        "colored-man-pages"
+        "mix"
+        "tmuxinator"
+        "docker-compose"
+        "docker"
+      ];
+    };
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+    shellAliases = {
+      myip = "curl -fSsL 'https://api.ipify.org?format=json' | jq \".ip\"";
+      ssh = "TERM=xterm-256color ssh";
+      cowalert = "xcowsay --monitor 1 comando: \" $(history | tail -n1 | grep -oP '\''(?<=  )[^;]++'\'' | head -n1) \" acabou ";
+      toclip = "xclip -selection clipboard";
+      granter = "cd /home/ferreira/dev/granter/";
+      ls = "exa";
+      ll = "exa -la";
+      ip = "ip -c";
+      ciasc-vpn = "sudo openfortivpn sslvpn01.ciasc.gov.br:443 --username=granter_gferreira@vpn.ciasc.gov.br";
+    };
+    initExtra = ''
+      _ssh_configfile()
+      {
+          set -- "''${words[@]}"
+          while [[ $# -gt 0 ]]; do
+              if [[ $1 == -F* ]]; then
+                  if [[ ''${#1} -gt 2 ]]; then
+                      configfile="$(dequote "''${1:2}")"
+                  else
+                      shift
+                      [[ $1 ]] && configfile="$(dequote "$1")"
+                  fi
+                  break
+              fi
+              shift
+          done
+      }
+      complete -F _ssh_configfile get-ssh-hostname
+
+      ### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+      pasteinit() {
+        OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+        zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+      }
+
+      pastefinish() {
+        zle -N self-insert $OLD_SELF_INSERT
+      }
+      zstyle :bracketed-paste-magic paste-init pasteinit
+      zstyle :bracketed-paste-magic paste-finish pastefinis
+    '';
+  };
+
+  # ---- Alacritty Configuration ---- #
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env = {
+        TERM = "xterm-256color";
+      };
+      shell.program = "${pkgs.zsh}/bin/zsh";
+      window = {
+        opacity = 0.85;
+        padding = {
+          x = 0;
+          y = 0;
+        };
+        class = {
+          instance = "Alacritty";
+          general = "Alacritty";
+        };
+      };
+      font = {
+        size = 10;
+        normal = {
+          font = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+      };
+      colors = {
+        primary = {
+          background = "0x000000";
+          foreground = "0xB3B1AD";
+        };
+        normal = {
+          black = "0x01060E";
+          red = "0xEA6C73";
+          green = "0x91B362";
+          yellow = "0xF9AF4F";
+          blue = "0x53BDFA";
+          magenta = "0xFAE994";
+          cyan = "0x90E1C6";
+          white = "0xC7C7C7";
+        };
+        bright = {
+          black = "0x686868";
+          red = "0xF07178";
+          green = "0xC2D94C";
+          yellow = "0xFFB454";
+          blue = "0x59C2FF";
+          magenta = "0xFFEE99";
+          cyan = "0x95E6CB";
+          white = "0xFFFFFF";
+        }
+      };
+    };
+  };
+
+  # ---- Starship Configuration ---- #
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  # TODO: Changed to .config/tmux config
+  # ---- tmux Configuration ---- #
+  programs.tmux = {
+    enable = true;
+    terminal = "tmux-256color";
+    baseIndex = 1;
+    historyLimit = 10000;
+    escapeTime = 10;
+    mouse = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+    tmuxinator.enable = true;
+    plugins = with pkgs; [
+      tmuxPlugins.tilish
+    ];
+    extraConfig = ''
+      set-option -ga terminal-overrides ",*256col*:Tc:RGB"
+    '';
+  };
+
+  # ---- tmuxinator Configuration ---- #
+  home.file.".config/tmuxinator" = {
+    source = ../config/tmuxinator;
+    recursive = true;
+  };
+
+  # TODO: Apply theme
+  # ---- rofi Configuration ---- #
+  programs.rofi = {
+    enable = true;
+  };
+
+  # ---- nvim Configuration ---- #
+  programs.neovim.enable = true;
+  home.file.".config/nvim" = {
+    source = ../config/nvim;
+    recursive = true;
+  };
+
+  # ---- Awesome Configuration ---- #
+  xsession.windowManager.awesome.enable = true;
+  home.file.".config/awesome" = {
+    source = ../config/awesome;
+    recursive = true;
+  };
+
+  # ---- Xresources colors :) ---- #
+  xresources.extraConfig = builtins.readFile (
+    pkgs.fetchFromGitHub {
+      owner = "rebelot";
+      repo = "kanagawa.nvim";
+      rev = "58cdce2cb666e6e946edec0145f177f89ca4a9ad";
+      sha256 = "sha256-TmuwIhjptegMcdwYToCTS9dyyndKzp5fJoahXF3F1K0=";
+    }
+    + "/extras/.Xresources"
+  );
+
+  # ---- SSH Configs ---- #
+  home.file.".ssh/config" = {
+    source = ../config/ssh/config;
+  };
+
+  # ---- Gnome Keyring Configs ---- #
+  services.picom = {
+    enable = true;
+    fade = true;
+    fadeSteps = [ 0.1 0.1 ];
+    shadow = true;
+    shadowOffsets = [ (-7) (-7) ];
+    shadowExclude = [
+      "name = 'Notification'"
+      "class_g = 'Conky'"
+      "class_g ?= 'Notify-osd'"
+      "class_g = 'Cairo-clock'"
+      "class_g = ''"
+      "_GTK_FRAME_EXTENTS@:c"
+    ];
+    inactiveOpacity = 0.9;
+    wintypes = {
+      tooltip = { fade = true; shadow = true; opacity = 0.75; focus = true; full-shadow = false; };
+      dock = { shadow = false; clip-shadow-above = true; };
+      dnd = { shadow = false; };
+      popup_menu = { shadow = false; fade = false; };
+    };
+    backend = "glx";
+    opacityRules = [
+      "100:name = 'Picture-in-Picture'"
+    ];
+    settings = {
+      inactive-opacity-override = false;
+      corner-radius = 8;
+      frame-opacity = 0.7;
+      rounded-corners-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+      ];
+      blur-kern = "3x3box";
+      blur-background-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+        "_GTK_FRAME_EXTENTS@:c"
+      ];
+      glx-no-stencil = true;
+      shadow-radius = 7;
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Materia-dark";
+      package = pkgs.materia-theme;
+    };
+  };
+
+  # ---- Secrets Management with SOPS ---- #
+  sops = {
+    age.sshKeyPaths = ["${config.home.homeDirectory}/.ssh/id_ed25519"];
+    defaultSopsFile = ../secrets/common/secrets.yaml;
+
+    secrets."ssh_keys/cbti/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/cbti";
+      mode = "0600";
+    };
+    secrets."ssh_keys/cbti/pub" = {
+      path = "${config.home.homeDirectory}/.ssh/cbti.pub";
+      mode = "0644";
+    };
+
+    secrets."ssh_keys/ciasc_root/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/ciasc_root";
+      mode = "0600";
+    };
+    secrets."ssh_keys/ciasc_root/pub" = {
+      path = "${config.home.homeDirectory}/.ssh/ciasc_root.pub";
+      mode = "0644";
+    };
+
+    secrets."ssh_keys/flowtify_pem/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/flowtify.pem";
+      mode = "0600";
+    };
+
+    secrets."ssh_keys/google_compute_engine/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/google_compute_engine";
+      mode = "0600";
+    };
+    secrets."ssh_keys/google_compute_engine/pub" = {
+      path = "${config.home.homeDirectory}/.ssh/google_compute_engine.pub";
+      mode = "0644";
+    };
+
+    secrets."ssh_keys/id_rsa/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/id_rsa";
+      mode = "0600";
+    };
+    secrets."ssh_keys/id_rsa/pub" = {
+      path = "${config.home.homeDirectory}/.ssh/id_rsa.pub";
+      mode = "0644";
+    };
+
+    secrets."ssh_keys/id_rsa_casa/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/id_rsa_casa";
+      mode = "0600";
+    };
+    secrets."ssh_keys/id_rsa_casa/pub" = {
+      path = "${config.home.homeDirectory}/.ssh/id_rsa_casa.pub";
+      mode = "0644";
+    };
+
+    secrets."ssh_keys/khor_linux_default_pem/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/khor-linux-default.pem";
+      mode = "0600";
+    };
+
+    secrets."ssh_keys/meulote_kp_pem/priv" = {
+      path = "${config.home.homeDirectory}/.ssh/meulote-kp.pem";
+      mode = "0600";
+    };
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,219 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    # overlays = [
+    # Add overlays your own flake exports (from overlays and pkgs dir):
+    # outputs.overlays.additions
+    # outputs.overlays.modifications
+    # outputs.overlays.unstable-packages
+
+    # You can also add overlays exported from other flakes:
+    # neovim-nightly-overlay.overlays.default
+
+    # Or define it inline, for example:
+    # (final: prev: {
+    #   hi = final.hello.overrideAttrs (oldAttrs: {
+    #     patches = [ ./change-hello-to-hi.patch ];
+    #   });
+    # })
+    # ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flakeconf.nix
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+  };
+
+  networking.hostName = "ferreira-pc";
+  networking.networkmanager.enable = true;
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = true;
+      # efiInstallAsRemovable = true;
+    };
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  security.pam.services.lightdm.enableGnomeKeyring = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+  services.gnome3.gnome-keyring.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["nvidia"];
+    displayManager = {
+      autoLogin.user = "ferreira";
+      lightdm.enable = true;
+      defaultSession = "none+i3";
+    };
+    windowManager.i3.enable = true;
+    layout = "us";
+    xkbVariant = "intl";
+    exportConfiguration = true;
+  };
+
+  services.xserver.displayManager.setupCommands = ''
+    LEFT='DP-2'
+    CENTER='DP-4'
+    RIGHT='DP-0'
+    ${pkgs.xorg.xrandr}/bin/xrandr --output $RIGHT --mode 1920x1080 --pos 3840x0 --rotate right \
+           --output $LEFT --mode 1920x1080 --pos 0x478 --rotate normal \
+           --output $CENTER --primary --mode 1920x1080 --pos 1920x478 --rotate normal --rate 143.98
+  '';
+
+  boot.kernelPackages = pkgs.linuxPackages_6_5;
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    # powerManagement.enable = false;
+    # powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  hardware.keyboard.qmk.enable = true;
+  environment.etc."ppp/options".text = "ipcp-accept-remote";
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    firefox
+    git
+    wget
+    curl
+    vim
+    neovim
+    home-manager
+    polkit_gnome
+    gnome.gnome-keyring
+  ];
+
+  users.users = {
+    ferreira = {
+      initialPassword = "1234";
+      isNormalUser = true;
+      description = "CABECA";
+      extraGroups = ["networkmanager" "wheel" "docker"];
+      packages = with pkgs; [
+        firefox
+        git
+        wget
+        curl
+        vim
+        neovim
+      ];
+    };
+  };
+
+  services.davfs2.enable = true;
+
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+  };
+
+  services.spice-vdagentd.enable = true;
+  virtualisation.docker.enable = true;
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+}
