b5d66a88f69eed6652435e9825456fe5e0421366
guihig <guiih.ig@gmail.com>
2024-05-07 09:29:58 -0300

    att

--- hosts/ferreira-pc/configuration.nix
+++ hosts/ferreira-pc/configuration.nix
@@ -126,7 +126,7 @@
     CENTER='DP-4'
     RIGHT='DP-0'
 
-    ${pkgs.xorg.xrandr}/bin/xrandr --output $LEFT --mode 3440x1440 --rate 144 --pos 0x358 --rotate normal  \
+    ${pkgs.xorg.xrandr}/bin/xrandr --output $LEFT --mode 3440x1440 --rate 144 --pos 0x0 --rotate normal  \
       --output $CENTER --primary --mode 1920x1080 --rate 240 --pos 3440x180 --dpi 102 --rotate normal \
       --output $RIGHT --mode 1920x1080 --pos 5360x180 --rotate normal
   '';
