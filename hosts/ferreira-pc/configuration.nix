4637da83083dd8da9db7cc07e3287e8bf4841792
guihig <guiih.ig@gmail.com>
2024-06-17 13:23:25 -0300

    att

--- hosts/ferreira-pc/configuration.nix
+++ hosts/ferreira-pc/configuration.nix
@@ -138,7 +138,7 @@
     modesetting.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   hardware.bluetooth = {
