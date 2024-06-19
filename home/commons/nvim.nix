5c91da403b99f9b737cae011acea03b50d61b8e4
guihig <guiih.ig@gmail.com>
2024-06-19 11:43:01 -0300

    att

--- home/commons/nvim.nix
+++ home/commons/nvim.nix
@@ -1,5 +1,12 @@
-{config, ...}: {
-  programs.neovim.enable = true;
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.neovim = {
+    enable = true;
+    package = pkgs.unstable.neovim-unwrapped;
+  };
   home.file.".config/nvim" = {
     source = config.lib.meta.mkMutableSymlink ../../config/nvim;
   };
