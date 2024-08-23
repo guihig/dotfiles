ab1f98d361925bec8ae995ad9fa4ee65570e1691
guihig <guiih.ig@gmail.com>
2024-08-23 11:41:47 -0300

    att

--- config/nvim/after/plugin/init_lspconfig.lua
+++ config/nvim/after/plugin/init_lspconfig.lua
@@ -50,6 +50,20 @@ local handlers = {
 					},
 				},
 			},
+			settings = {
+				typescript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "all",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+						includeInlayFunctionParameterTypeHints = true,
+						includeInlayVariableTypeHints = true,
+						includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+						includeInlayPropertyDeclarationTypeHints = true,
+						includeInlayFunctionLikeReturnTypeHints = true,
+						includeInlayEnumMemberValueHints = true,
+					},
+				},
+			},
 		})
 	end,
 	["elixirls"] = function()
@@ -96,6 +110,29 @@ local handlers = {
 			init_options = {
 				vue = {
 					hybridMode = true,
+				},
+			},
+
+			settings = {
+				typescript = {
+					inlayHints = {
+						enumMemberValues = {
+							enabled = true,
+						},
+						functionLikeReturnTypes = {
+							enabled = true,
+						},
+						propertyDeclarationTypes = {
+							enabled = true,
+						},
+						parameterTypes = {
+							enabled = true,
+							suppressWhenArgumentMatchesName = true,
+						},
+						variableTypes = {
+							enabled = true,
+						},
+					},
 				},
 			},
 			-- capabilities = {
