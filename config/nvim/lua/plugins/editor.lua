7fe3752a09ae65823a374637a12e10df8d9cbf42
guihig <guiih.ig@gmail.com>
2024-08-25 16:54:38 -0300

    att

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -14,8 +14,30 @@ return {
 	{
 		"ggandor/leap.nvim",
 		config = function()
-			keymap.set({ "n", "x", "o" }, "<leader>s", "<Plug>(leap)")
-			keymap.set({ "n", "x", "o" }, "<leader>S", "<Plug>(leap-from-window)")
+			vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+			keymap.set({ "n", "x", "o", "v" }, "<leader>s", "<Plug>(leap)")
+			keymap.set({ "n", "x", "o", "v" }, "<leader>S", "<Plug>(leap-from-window)")
+			-- Hide the (real) cursor when leaping, and restore it afterwards.
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LeapEnter",
+				callback = function()
+					vim.cmd.hi("Cursor", "blend=100")
+					vim.opt.guicursor:append({ "a:Cursor/lCursor" })
+				end,
+			})
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LeapLeave",
+				callback = function()
+					vim.cmd.hi("Cursor", "blend=0")
+					vim.opt.guicursor:remove({ "a:Cursor/lCursor" })
+				end,
+			})
+		end,
+	},
+	{
+		"ggandor/flit.nvim",
+		config = function()
+			require("flit").setup()
 		end,
 	},
 }
