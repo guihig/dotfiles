6f3ae1ed0fd4e23e2fd861fb93a8ba4186917972
guihig <guiih.ig@gmail.com>
2024-08-25 21:49:17 -0300

    att

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -1,13 +1,31 @@
 local keymap = vim.keymap
 return {
-	{ "echasnovski/mini.move", version = "*" },
-	{ "echasnovski/mini.surround", version = "*" },
-	{ "echasnovski/mini.cursorword", version = "*" },
-	{ "echasnovski/mini.animate", version = "*" },
+	{
+		"echasnovski/mini.move",
+		version = "*",
+		config = function()
+			require("mini.move").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		version = "*",
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.cursorword",
+		version = "*",
+		config = function()
+			require("mini.cursorword").setup()
+		end,
+	},
 	{
 		"echasnovski/mini.bufremove",
 		version = "*",
 		config = function()
+			require("mini.bufremove").setup()
 			local keymap_opts = { silent = true, noremap = true }
 			keymap.set("n", "<A-w>", ":bp|bd #<CR>", keymap_opts)
 		end,
