03a954dce64a93036c6483b84049aa5d3b9edb07
guihig <guiih.ig@gmail.com>
2024-08-26 12:39:12 -0300

    att

--- config/nvim/lua/plugins/typescript_walking_stick.lua
+++ config/nvim/lua/plugins/typescript_walking_stick.lua
@@ -8,10 +8,33 @@ return {
 		end,
 	},
 	{ "catgoose/vue-goto-definition.nvim" },
+	-- {
+	-- 	"dmmulroy/ts-error-translator.nvim",
+	-- 	config = true,
+	-- 	ft = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
+	-- },
 	{
-		"dmmulroy/ts-error-translator.nvim",
-		config = true,
-		ft = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
+		"OlegGulevskyy/better-ts-errors.nvim",
+		config = {
+			keymaps = {
+				toggle = "<leader>dte",
+				go_to_definition = "<leader>dx",
+			},
+		},
 	},
-	{ "JoosepAlviste/nvim-ts-context-commentstring" },
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
+			local native_get_option = vim.filetype.get_option
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.filetype.get_option = function(filetype, option)
+				return option == "commentstring"
+						and require("ts_context_commentstring.internal").calculate_commentstring()
+					or native_get_option(filetype, option)
+			end
+		end,
+	},
 }
