2995396f079f811d5c0825f63bf2b6d524e999f5
guihig <guiih.ig@gmail.com>
2024-08-30 15:43:04 -0300

    att

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,5 +1,10 @@
 return {
 	{ "lewis6991/gitsigns.nvim" },
 	{ "tpope/vim-fugitive" },
-	{ "akinsho/git-conflict.nvim" },
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		opts = {},
+		config = true,
+	},
 }
