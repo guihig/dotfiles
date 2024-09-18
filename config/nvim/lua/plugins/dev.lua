3c0436ea3dc34fb11a650448b01b776f37ee7a08
guihig <guiih.ig@gmail.com>
2024-09-18 13:33:44 -0300

    att

--- config/nvim/lua/plugins/dev.lua
+++ config/nvim/lua/plugins/dev.lua
@@ -11,6 +11,20 @@ return {
 			},
 		},
 	},
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		keys = {
+			{ "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+			{ "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+		},
+		opts = {
+			save_path = "~/Pictures",
+			has_breadcrumbs = true,
+			has_line_number = true,
+			bg_padding = 0,
+		},
+	},
 	{ "b0o/schemastore.nvim" },
 	{
 
