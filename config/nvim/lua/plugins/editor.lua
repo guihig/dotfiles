236c09bb085e3a21cef8316e84740ef1311745f0
guihig <guiih.ig@gmail.com>
2025-04-02 17:15:27 -0300

    att

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,25 @@
 local keymap = vim.keymap
+
+function highlight_unlabeled_phase_one_targets(targets, first_idx, last_idx)
+	local hl = require("leap.highlight")
+	for i = first_idx or 1, last_idx or #targets do
+		local target = targets[i]
+		if not target.label and target.chars then
+			local bufnr = target.wininfo.bufnr
+			local id = vim.api.nvim_buf_set_extmark(bufnr, hl.ns, target.pos[1] - 1, target.pos[2] - 1, {
+				virt_text = { { table.concat(target.chars), "LeapMatch" } },
+				virt_text_pos = "overlay",
+				hl_mode = "combine",
+				priority = hl.priority.label,
+			})
+			-- This way Leap automatically cleans up your stuff together with its own.
+			table.insert(hl.extmarks, { bufnr, id })
+		end
+	end
+	-- Continue with Leap's native function body.
+	return true
+end
+
 return {
 	{ "tpope/vim-repeat" },
 	{ "psliwka/vim-smoothie" },
@@ -15,6 +36,13 @@ return {
 		"ggandor/leap.nvim",
 		config = function()
 			vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+			vim.api.nvim_set_hl(0, "LeapMatch", {
+				fg = "white",
+				bold = true,
+				nocombine = true,
+			})
+			-- vim.api.nvim_set_hl(0, "LeapLabelPrimary", { bg = "green" })
+			-- vim.api.nvim_set_hl(0, "LeapLabelSecondary", { bg = "blue" })
 			keymap.set({ "n", "x", "o", "v" }, "<leader>s", "<Plug>(leap)")
 			keymap.set({ "n", "x", "o", "v" }, "<leader>S", "<Plug>(leap-from-window)")
 			-- Hide the (real) cursor when leaping, and restore it afterwards.
