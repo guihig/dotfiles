277419e4401037dad974585f5f8b44f18182f748
guihig <guiih.ig@gmail.com>
2026-07-01 16:52:49 -0300

    att

--- modules/programs/nvim/configs/lua/plugins/treesitter.lua
+++ modules/programs/nvim/configs/lua/plugins/treesitter.lua
@@ -1,44 +1,126 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	commit = "90cd658",
+	main = "nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = { enable = true },
-			incremental_selection = { enable = true },
-			indent = { enable = true },
-			sync_install = false,
-			auto_install = true,
-			ignore_install = {},
-			ensure_installed = {
-				"haskell",
-				"lua",
-				"json",
-				"css",
-				"html",
-				"vue",
-				"http",
-				"typescript",
-				"qmljs",
-				"heex",
-				-- "latex",
-				"fish",
-				"vim",
-				"elixir",
-				"scss",
-				"markdown",
-				"markdown_inline",
-				"regex",
-				"bash",
-				"vimdoc",
-			},
+	event = { "BufReadPost", "BufNewFile" },
+	init = function()
+		local highlight = function(bufnr, lang)
+			-------------------[ treesitter highlights ]-------------------------------
+			if not vim.treesitter.language.add(lang) then
+				return vim.notify(
+					string.format("Treesitter cannot load parser for language: %s", lang),
+					vim.log.levels.INFO,
+					{ title = "Treesitter" }
+				)
+			end
+			vim.treesitter.start(bufnr)
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local ft = vim.bo.filetype
+				local bt = vim.bo.buftype
+				local buf = args.buf
+
+				if bt ~= "" then
+					return
+				end -- don't run further.
+
+				local ok, treesitter = pcall(require, "nvim-treesitter")
+				if not ok then
+					return
+				end
+
+				--------------------[ treesitter folds ]-------------------------------
+
+				if ft == "javascriptreact" or ft == "typescriptreact" then
+					vim.opt_local.foldmethod = "indent"
+				else
+					vim.opt_local.foldmethod = "expr"
+					vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				end
+
+				vim.schedule(function()
+					-- Only run normal if we're not in terminal mode
+					if vim.fn.mode() ~= "t" then
+						vim.cmd("silent! normal! zx")
+					end
+				end)
+
+				---------------------[ treesitter indent ]-------------------------------
+
+				if not vim.tbl_contains({ "python", "html", "yaml", "markdown" }, ft) then
+					vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+				end
+
+				--------------------[ treesitter parsers ]-------------------------------
+				if vim.fn.executable("tree-sitter") ~= 1 then
+					vim.api.nvim_echo({
+						{
+							"tree-sitter CLI not found. Parsers cannot be installed.",
+							"ErrorMsg",
+						},
+					}, true, {})
+					return false
+				end
+
+				if not vim.treesitter.language.get_lang(ft) then
+					return
+				end
+
+				if vim.list_contains(treesitter.get_installed(), ft) then
+					highlight(buf, ft)
+				elseif vim.list_contains(treesitter.get_available(), ft) then
+					treesitter.install(ft):await(function()
+						highlight(buf, ft)
+					end)
+				end
+			end,
 		})
+	end,
+	opts = {
+		install = {
+
+			"haskell",
+			"lua",
+			"json",
+			"css",
+			"html",
+			"vue",
+			"http",
+			"typescript",
+			"qmljs",
+			"heex",
+			-- "latex",
+			"fish",
+			"vim",
+			"elixir",
+			"scss",
+			"markdown",
+			"markdown_inline",
+			"regex",
+			"bash",
+			"vimdoc",
+		},
+	},
+	config = function(_, opts)
+		local treesitter = require("nvim-treesitter")
+		treesitter.setup(opts)
+		if vim.fn.executable("tree-sitter") ~= 1 then
+			vim.api.nvim_echo({
+				{
+					"tree-sitter CLI not found. Parsers cannot be installed.",
+					"ErrorMsg",
+				},
+			}, true, {})
+			return false
+		end
+		treesitter.install(opts.install)
 
 		require("ts_context_commentstring").setup({
 			enable_autocmd = false,
 		})
-
 		vim.g.skip_ts_context_commentstring_module = true
-		vim.opt.foldmethod = "expr"
-		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 	end,
 }
